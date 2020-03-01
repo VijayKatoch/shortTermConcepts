270d8cee36d5378e5f880296452d5202c206466d
Katoch <Katoch@DESKTOP-HGTMRDU.local>
2020-03-01 21:14:30 +0100

    method added

--- stm/systemWorkbench/drive_control/motor_rpm_node.cpp
+++ stm/systemWorkbench/drive_control/motor_rpm_node.cpp
@@ -5,6 +5,24 @@
  *      Author: Vijay Katoch
  */
 
+#include "motor_rpm_node.h"
 
+using namespace KATBOT;
+
+MotorRPMNode::MotorRPMNode()
+{
+
+}
+
+void MotorRPMNode::encoderCallback()
+{
+
+}
+
+float MotorRPMNode::rpmFromEncoderCount()
+{
+
+  return 0.0;
+}
 
 
